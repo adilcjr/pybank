023541a6fcff437cd885ff42532cb13e0021414d
Adil Calomeno Junior <adil.calomeno.jr@gmail.com>
2023-06-01 09:09:44 -0300

    Initial tests of data persistance

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
 """API routes"""
-from flask import render_template, request
+from flask import render_template, request, jsonify
+from models.account import Account, db
 
 
 def init_routes(app):
@@ -21,19 +22,27 @@ def init_routes(app):
 
         account_id = request.args.get("account_id", 0, int)
 
-        if account_id == 100:
-            return "20"
+        if account_id > 0:
+            account = Account.query.get(account_id)
+            return jsonify(
+                {"destination": {"id": account.id, "balance": account.balance}}
+            )
         return "0", 404
 
     @app.post("/event")
     def event():
-        """Transfer from existing account"""
+        """Deal with account transactions: deposit, transfer and withdraw"""
 
         trx_type = request.json["type"]
 
         if trx_type == "deposit":
             destination = request.json["destination"]
             amount = request.json["amount"]
+
+            account = Account(destination, amount)
+            db.session.add(account)
+            db.session.commit()
+
             return {"destination": {"id": destination, "balance": amount}}, 201
 
         if trx_type == "transfer":
