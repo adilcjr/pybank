a4248ad803cd41273f61a8fe39a3b99fb299df39
Adil Calomeno Junior <adil.calomeno.jr@gmail.com>
2023-06-01 00:28:54 -0300

    Docstring added and unused imports were removed

--- api/init.py
+++ api/init.py
@@ -1,14 +1,16 @@
-from flask import Flask, jsonify
+""" Module starter"""
+import os
+from flask import Flask
 
 from api.routes import init_routes
 
 
 def create_app(test_config=None):
-    # creates an application that is named after the name of the file
+    """Creates the application"""
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = "some_dev_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://usr:pwd@pgsql:5432/todos"
+    app.config["SECRET_KEY"] = "my_super_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
     # initializing routes
     init_routes(app)
